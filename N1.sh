cdf8d0800da5863570f13ffd8f870721f0219f8a
8688Add <42204083+8688Add@users.noreply.github.com>
2021-03-24 00:40:13 +0800

    Delete N1.sh

--- N1.sh
+++
@@ -1,9 +0,0 @@
-#修改版本号
-#sed -i 's/OpenWrt/BGG $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
-svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns-le package/smartdns-le
-git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
-git clone https://github.com/tuanqing/install-program package/install-program
